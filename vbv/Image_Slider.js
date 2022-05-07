22f9f0293eec760096771b3af9ea6cd2dd1d35a8
AadishJain249 <87666139+AadishJain249@users.noreply.github.com>
2022-05-07 17:23:30 +0530

    commit

--- vbv/Image_Slider.js
+++ vbv/Image_Slider.js
@@ -13,8 +13,23 @@ show_slide = (i) => {
     images[index].style.display = "block";
 }
 
+let index1 = 0;
+show_slide1 = (j) => {
+    index1 += j;
+    var images1 = document.getElementsByClassName("image1");
+
+    for (j = 0; j < images1.length; j++)
+        images1[j].style.display = "none";
+
+    if (index1 > images1.length - 1) index1 = 0;
+    else if (index1 < 0) index1 = images1.length - 1;
+
+    images1[index1].style.display = "block";
+}
+
 // setInterval(() => {
 //     show_slide(index++);
 // }, 3000);
 
 window.addEventListener("onload", show_slide(index));
+window.addEventListener("onload", show_slide1(index1));
